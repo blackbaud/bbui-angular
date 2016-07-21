704f74d87537f18d1b848cdf078bc636d1407f8d
Christine Viets <christine.viets@blackbaud.com>
2016-07-21 17:19:55 -0400

    Docs for core

--- src/core.js
+++ src/core.js
@@ -40,15 +40,15 @@
             }
 
             /**
-             * @class BBUI Contains methods for issuing XMLHttpReqests as well as some basic helper functions.
+             * @class bbui
+             * Contains methods for issuing XMLHttpReqests as well as some basic helper functions.
              * @singleton
-             * @uimodel <span style="color: green;">Yes</span>
-             * @pageaction <span style="color: green;">Yes</span>
-            */
+             */
             BBUI = {
 
                 /**
-                 * Read-only.  Represents an empty GUID value.
+                 * @readonly
+                 * Represents an empty GUID value.
                  * @type {String}
                  */
                 emptyGuid: "00000000-0000-0000-0000-000000000000",
@@ -56,37 +56,42 @@
                 /**
                  * Returns a flag indicating the given object is defined and its value is not null.   This function is mainly
                  * used to see if a JSON property is present, since the absense of a property usually means "no change" rather
-                 * than "this property's value was changed to null."<br/><br/>
+                 * than "this property's value was changed to null."
+                 * <br/><br/>
                  * Calling this function is equivalent to evaluating <tt>typeof obj !== "undefined" && obj !== null</tt>.
-                 * @param {Object} obj The object to check.
-                 * @return {Boolean} False if the object is null or undefined; otherwise, true.
+                 *
+                 * @param {Object} obj
+                 * The object to check.
+                 *
+                 * @return {Boolean}
+                 * False if the object is null or undefined; otherwise, true.
                  */
                 is: function (obj) {
-                    /// <summary>
-                    /// Returns a flag indicating the given object is defined and its value is not null.   This function is mainly
-                    /// used to see if a JSON property is present, since the absense of a property usually means "no change" rather
-                    /// than "this property's value was changed to null."
-                    /// Calling this function is equivalent to evaluating "typeof obj !== 'undefined' && obj !== null".
-                    /// </summary>
-                    /// <param name="obj" type="Object">The object to check.</param>
                     return typeof obj !== "undefined" && obj !== null;
                 },
 
                 /**
                  * Finds an item in an array with the specified property value.
-                 * @param {Array} items The array to search.
-                 * @param {String} propName The property's name.
-                 * @param {Object} value The property's value.
-                 * @param {Boolean} ignorePropCase (optional) Does a case-insensitive search on the property's name.  Default is false.
-                 * @param {Boolean} ignoreValueCase (optional) Does a case-insensitive search on the property's value.  Default is false.
-                 * @return {Object} The first item in the array that matches the property value (or null if no matching item is found).
+                 *
+                 * @param {Array} items
+                 * The array to search.
+                 *
+                 * @param {String} propName
+                 * The property's name.
+                 *
+                 * @param {Object} value
+                 * The property's value.
+                 *
+                 * @param {Boolean} [ignorePropCase=false]
+                 * Does a case-insensitive search on the property's name.
+                 *
+                 * @param {Boolean} [ignoreValueCase=false]
+                 * Does a case-insensitive search on the property's value.
+                 *
+                 * @return {Object}
+                 * The first item in the array that matches the property value (or null if no matching item is found).
                  */
                 findByProp: function (items, propName, value, ignorePropCase, ignoreValueCase) {
-                    /// <summary>Finds an item in an array with the specified property value.</summary>
-                    /// <param name="items" type="Array">The array to search.</param>
-                    /// <param name="propName" type="String">The name of the property.</param>
-                    /// <param name="value" type="Object">The value of the property.</param>
-                    /// <returns type="object">The first item in the array that matches the property value (or null if no matching item is found).</returns>
 
                     var i,
                         item,
@@ -109,12 +114,24 @@
                     return null;
                 },
 
+                /**
+                 * Gets an object's property value using a case-insensitive comparison if specified.
+                 *
+                 * @param {Object} obj
+                 * The object containing the property.
+                 *
+                 * @param {String} propName
+                 * The name of the property.
+                 *
+                 * @param {Boolean} ignoreCase
+                 * Indicates whether to ignore case on the property value.
+                 *
+                 * @param {Object} defaultValue
+                 * The default value to return if the property does not exist.
+                 *
+                 * @return {Object}
+                 */
                 getPropValue: function (obj, propName, ignoreCase, defaultValue) {
-                    /// <summary>Gets an object's property value using a case-insensitive comparison if specified.</summary>
-                    /// <param name="obj" type="Object">The object containing the property.</param>
-                    /// <param name="propName" type="String">The name of the property.</param>
-                    /// <param name="ignoreCase" type="Boolean">Indicates whether to ignore case on the property value.</param>
-                    /// <param name="defaultValue" type="Object">The default value to return if the property does not exist.</param>
 
                     var p,
                         propNameUpper;
@@ -138,10 +155,16 @@
                     return defaultValue;
                 },
 
+                /**
+                 * Copies the properties of the specified object to a new object and returns the new object.
+                 *
+                 * @param {Object} obj
+                 * Object to clone.
+                 *
+                 * @return {Object}
+                 * The cloned object.
+                 */
                 clone: function (obj) {
-                    /// <summary>Copies the properties of the specified object to a new object and returns the new object.</summary>
-                    /// <param name="obj" type="Object">Object to clone.</param>
-                    /// <returns type="Object">The cloned object.</return>
 
                     var cloneObj,
                         p;
@@ -161,6 +184,8 @@
                     return cloneObj;
                 },
 
+                /**
+                 */
                 copyProps: function (to, from) {
                     var p;
 
@@ -175,31 +200,27 @@
 
                 /**
                  * Compares two GUID values by normalizing capitalization of each GUID and comparing them as strings.
-                 * @param {String} guid1 The first GUID to compare.
-                 * @param {String} guid2 The second GUID to compare.
-                 * @param {String} guid1Upper (optional) A flag indicating the first GUID is already upper-cased.  When true, a new upper-cased string is not created for the
+                 *
+                 * @param {String} guid1
+                 * The first GUID to compare.
+                 *
+                 * @param {String} guid2
+                 * The second GUID to compare.
+                 *
+                 * @param {String} [guid1Upper]
+                 * A flag indicating the first GUID is already upper-cased.  When true, a new upper-cased string is not created for the
                  * first GUID for comparison.  Use this flag as an optimization when comparing GUIDs in a loop so a new string does not
                  * have to be created for each iteration of the loop when the GUID does not change between iterations.
-                 * @param {String} guid2Upper (optional) A flag indicating the second GUID is already upper-cased.  When true, a new upper-cased string is not created for the
+                 *
+                 * @param {String} [guid2Upper]
+                 * A flag indicating the second GUID is already upper-cased.  When true, a new upper-cased string is not created for the
                  * second GUID for comparison.  Use this flag as an optimization when comparing GUIDs in a loop so a new string does not
                  * have to be created for each iteration of the loop when the GUID does not change between iterations.
-                 * @return {Boolean} A flag indicating whether the two GUIDs are the same.
+                 *
+                 * @return {Boolean}
+                 * A flag indicating whether the two GUIDs are the same.
                  */
                 guidEquals: function (guid1, guid2, guid1Upper, guid2Upper) {
-                    /// <summary>Compares two GUID values by normalizing capitalization of each GUID and comparing them as strings.</summary>
-                    /// <param name="guid1" type="String">The first GUID to compare.</param>
-                    /// <param name="guid2" type="String">The second GUID to compare.</param>
-                    /// <param name="guid1Upper" type="Boolean" optional="true">
-                    /// A flag indicating the first GUID is already upper-cased.  When true, a new upper-cased string is not created for the
-                    /// first GUID for comparison.  Use this flag as an optimization when comparing GUIDs in a loop so a new string does not
-                    /// have to be created for each iteration of the loop when the GUID does not change between iterations.
-                    /// </param>
-                    /// <param name="guid2Upper" type="Boolean" optional="true">
-                    /// A flag indicating the second GUID is already upper-cased.  When true, a new upper-cased string is not created for the
-                    /// second GUID for comparison.  Use this flag as an optimization when comparing GUIDs in a loop so a new string does not
-                    /// have to be created for each iteration of the loop when the GUID does not change between iterations.
-                    /// </param>
-                    /// <returns type="Boolean">A flag indicating whether the two GUIDs are the same.</returns>
 
                     if (typeof guid1 !== TYPE_STRING || typeof guid2 !== TYPE_STRING) {
                         return false;
@@ -216,10 +237,16 @@
                     return guid1 === guid2;
                 },
 
+                /**
+                 * Parses the provided object name and returns a reference to the object it represents.
+                 *
+                 * @param {String} objName
+                 * The name of the object.
+                 *
+                 * @return {Object}
+                 * The corresponding object, or null if any part of the object is undefined.
+                 */
                 getObjByName: function (objName) {
-                    /// <summary>Parses the provided object name and returns a reference to the object it represents.</summary>
-                    /// <param name="objName" type="String">The name of the object.</param>
-                    /// <returns type="Object">The corresponding object, or null if any part of the object is undefined.</returns>
 
                     var i,
                         n,
@@ -249,40 +276,14 @@
                 },
 
                 /**
-                 * Creates a "namespace" using the provided string and returns the object representing the namespace.
-                 * An example where a custom page action is created under the BBUI.customactions namespace:
-                 * <pre><code>
-        var ns;
-
-        // Ensure the webshelltest namespace exists.
-        ns = BBUI.ns("BBUI.customactions.webshelltest");
-
-        ns.MyCustomAction = function (host) {
-            // Cache the host object so it can be referenced later.
-            this.host = host;
-        };
-
-        ns.MyCustomAction.prototype = {
-
-            executeAction: function (callback) {
-                // Action logic goes here.
-            }
-
-        };
-
-                /**
                  * Concatenates all the provided arguments as if they were portions of a URL, inserting forward slashes where appropriate.
                  * @param {String} arg1 A portion of the URL.
-                 * @param {String} arg2 (optional)
-                 * @param {String} etc (optional)
+                 * @param {String} [arg2]
+                 * @param {String} [argN]
+                 *
                  * @return {String} The concatenated URL.
                  */
                 urlConcat: function (arg1) {
-                    /// <summary>
-                    /// Concatenates all the provided arguments as if they were portions of a URL, inserting forward slashes
-                    /// where appropriate.
-                    /// </summary>
-                    /// <returns type="String">The concatenated URL.</returns>
 
                     var arg,
                         argCount,
@@ -321,31 +322,40 @@
                     return null;
                 },
 
+                /**
+                 * Takes the special arguments object from a function and returns the arguments in a true array.
+                 *
+                 * @param {Object} args
+                 * The arguments object.
+                 *
+                 * @param {Number} [start]
+                 * The index of the first item to return.  When not specified, all the items are returned.
+                 *
+                 * @return {Object[]}
+                 * The array of arguments.
+                 */
                 argsToArray: function (args, start) {
-                    /// <summary>
-                    /// Takes the special arguments object from a function and returns the arguments in a true array.
-                    /// </summary>
-                    /// <param name="args">The arguments object.</param>
-                    /// <param name="start" type="Integer" optional="true">
-                    /// The index of the first item to return.  When not specified, all the items are returned.
-                    /// </param>
-                    /// <returns type="Array">The array of arguments.</returns>
                     return Array.prototype.slice.call(args, start || 0);
                 },
 
+                /**
+                 * Takes an array of objects with an "id" or "name" property and a "value" property and returns
+                 * the items as a query string.
+                 *
+                 * @param {Object[]} items
+                 * The array of objects.
+                 *
+                 * @param {String} [itemPrefix]
+                 * The string to prepend to the query string item name.
+                 *
+                 * @param {Boolean} [prependAmpersand]
+                 * Flag indicating whether to prepend an ampersand to the returned query string.
+                 *
+                 * @return {String}
+                 * The query string.
+                 */
                 arrayToQueryString: function (items, itemPrefix, prependAmpersand) {
-                    /// <summary>
-                    /// Takes an array of objects with an "id" or "name" property and a "value" property and returns
-                    /// the items as a query string.
-                    /// <summary>
-                    /// <param name="items" type="Array">The array of objects.</param>
-                    /// <param name="itemPrefix" type="String" use="optional">
-                    /// The string to prepend to the query string item name.
-                    /// </param>
-                    /// <param name="prependAmpersand" type="Boolean" use="optional">
-                    /// Flag indicating whether to prepend an ampersand to the returned query string.
-                    /// </param>
-                    /// <returns type="String">The query string.</returns>
+
                     var i,
                         item,
                         n,
@@ -370,6 +380,9 @@
                     return s;
                 },
 
+                /**
+                 * @return {String}
+                 */
                 getAbsoluteBaseUrl: function (relativeBaseUrl) {
                     var baseUrl,
                         i,
@@ -409,9 +422,15 @@
 
                 /**
                  * Detemines whether the value of the first parameter ends with the value of the second parameter.
-                 * @param {String} s The value to search.
-                 * @param {String} val The value to find.
-                 * @return {Boolean} A flag indicating whether the value of the first parameter ends with the value of the second parameter.
+                 *
+                 * @param {String} s
+                 * The value to search.
+                 *
+                 * @param {String} val
+                 * The value to find.
+                 *
+                 * @return {Boolean}
+                 * A flag indicating whether the value of the first parameter ends with the value of the second parameter.
                  */
                 endsWith: function (s, val) {
                     var pos;
@@ -427,9 +446,15 @@
                 /**
                  * Overrides functions on the first argument with properties from the second argument and returns an object
                  * with the original base functions.
-                 * @param {Object} to The object whose functions are to be overridden.
-                 * @param {Object} from The Object containing the override functions.
-                 * @return {Object} The object containing the original functions.
+                 *
+                 * @param {Object} to
+                 * The object whose functions are to be overridden.
+                 *
+                 * @param {Object} from
+                 * The Object containing the override functions.
+                 *
+                 * @return {Object}
+                 * The object containing the original functions.
                  */
                 override: function (to, from) {
                     var base,
@@ -453,6 +478,9 @@
                     return base;
                 },
 
+                /**
+                 * @return {String}
+                 */
                 escapeRegExp: function (filter) {
                     if (typeof filter === TYPE_STRING) {
                         return filter.replace(escapeRegExpRegEx, '\\$&');
